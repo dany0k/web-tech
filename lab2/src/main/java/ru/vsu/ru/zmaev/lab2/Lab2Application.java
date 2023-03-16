3b52256b82c4cbe0a4d0f61f4804e89914d40f84
Den-Laptop <zmaev.vsu@gmail.com>
2023-03-16 10:06:00 +0300

    Update main class

--- lab2/src/main/java/ru/vsu/ru/zmaev/lab2/Lab2Application.java
+++ lab2/src/main/java/ru/vsu/ru/zmaev/lab2/Lab2Application.java
@@ -2,6 +2,7 @@ package ru.vsu.ru.zmaev.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,9 +10,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Lab2Application {
     public static void main(String[] args) {
-
         SpringApplication.run(Lab2Application.class, args);
+        System.out.println("Hello");
     }
 }
