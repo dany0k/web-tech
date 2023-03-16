6fdf32db00a9e7eb2718a3c98a2d765518999435
DenPC <zmaev.vsu@gmail.com>
2023-03-16 03:16:49 +0300

    Update PairsRepository

--- lab2/src/main/java/ru/vsu/ru/zmaev/lab2/repositories/PairsRepository.java
+++ lab2/src/main/java/ru/vsu/ru/zmaev/lab2/repositories/PairsRepository.java
@@ -1,5 +1,17 @@
 package ru.vsu.ru.zmaev.lab2.repositories;
 
-public interface PairsRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.relational.core.sql.In;
+import org.springframework.data.repository.CrudRepository;
+import ru.vsu.ru.zmaev.lab2.models.Pairs;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface PairsRepository extends CrudRepository<Pairs, Integer> {
+    List<Pairs> findAll();
+
+    Optional<Pairs> findByPairsId(Integer id);
+    @Query(value = "SELECT p FROM pairs WHERE p.group_id = ?1")
+    List<Pairs> findByStudentGroupId(Integer groupId);
 }
