0d55de17e0f815ab6edebbb9490aeecfd5a0ee32
Den-Laptop <zmaev.vsu@gmail.com>
2023-03-17 17:58:50 +0300

    Complete Subject

--- lab2/src/main/java/ru/vsu/ru/zmaev/lab2/controllers/SubjectController.java
+++ lab2/src/main/java/ru/vsu/ru/zmaev/lab2/controllers/SubjectController.java
@@ -1,8 +1,9 @@
 package ru.vsu.ru.zmaev.lab2.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.minidev.json.JSONObject;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.vsu.ru.zmaev.lab2.models.Subject;
 import ru.vsu.ru.zmaev.lab2.services.SubjectService;
 
@@ -20,5 +21,49 @@ public class SubjectController {
     @GetMapping("/subject")
     public List<Subject> findAllSubjects() {
         return service.getAllSubjects();
+    }
+
+    @GetMapping("/subject/{id}")
+    public ResponseEntity<?> findBySubjectId(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(service.getSubjectById(id));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @PostMapping("/subject/create")
+    public ResponseEntity<?> createSubject(@RequestBody Subject subject) {
+        try {
+            return ResponseEntity.ok(service.addSubject(subject));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @PutMapping("/subject/{id}/update")
+    public ResponseEntity<?> updateSubject(@PathVariable Integer id, @RequestBody Subject subject) {
+        try {
+            return ResponseEntity.ok(service.updateSubject(id, subject));
+        } catch (IllegalIdentifierException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @DeleteMapping("/subject/{id}/delete")
+    public ResponseEntity<?> deleteSubject(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(service.deleteSubject(id));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
     }
 }
--- lab2/src/main/java/ru/vsu/ru/zmaev/lab2/services/SubjectService.java
+++ lab2/src/main/java/ru/vsu/ru/zmaev/lab2/services/SubjectService.java
@@ -1,5 +1,7 @@
 package ru.vsu.ru.zmaev.lab2.services;
 
+import org.jvnet.staxex.BinaryText;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.vsu.ru.zmaev.lab2.models.Subject;
 import ru.vsu.ru.zmaev.lab2.repositories.SubjectRepository;
@@ -30,5 +32,25 @@ public class SubjectService {
     public Subject getSubjectByName(String name) {
         return repository.findByName(name).orElseThrow(() ->
                 new IllegalArgumentException("Nu such subject"));
+    }
+
+    public Subject addSubject(Subject subject) {
+        return repository.save(subject);
+    }
+
+    public Subject updateSubject(Integer id, Subject subject) throws IllegalArgumentException {
+        return repository.findById(id).map(s -> {
+            s.setSubjectId(subject.getSubjectId());
+            s.setName(subject.getName());
+            s.setDescription(subject.getDescription());
+            return repository.save(s);
+        }).orElseThrow(() -> new IllegalArgumentException("No such subject"));
+    }
+
+    public ResponseEntity<?> deleteSubject(Integer id) throws IllegalArgumentException {
+        return repository.findById(id).map(s -> {
+            repository.delete(s);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new IllegalArgumentException("No such subject"));
     }
 }
