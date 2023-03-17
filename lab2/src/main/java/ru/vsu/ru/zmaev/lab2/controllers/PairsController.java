bc155ba6c8f6f0b1c3fdd9b3c5233669f6a15f62
Den-Laptop <zmaev.vsu@gmail.com>
2023-03-17 18:14:18 +0300

    Complete Pairs

--- lab2/src/main/java/ru/vsu/ru/zmaev/lab2/controllers/PairsController.java
+++ lab2/src/main/java/ru/vsu/ru/zmaev/lab2/controllers/PairsController.java
@@ -1,9 +1,11 @@
 package ru.vsu.ru.zmaev.lab2.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.minidev.json.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.vsu.ru.zmaev.lab2.models.Group;
 import ru.vsu.ru.zmaev.lab2.models.Pairs;
+import ru.vsu.ru.zmaev.lab2.models.Subject;
 import ru.vsu.ru.zmaev.lab2.services.PairsService;
 
 import java.util.List;
@@ -21,4 +23,60 @@ public class PairsController {
     public List<Pairs> getAllPairs() {
         return service.getAllPairs();
     }
+
+    @GetMapping("/pairs/subject/{id}")
+    public ResponseEntity<?> getPairsBySubjectId(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(service.getPairBySubjectId(id));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @GetMapping("/pairs/group/{id}")
+    public ResponseEntity<?> getPairsByGroupId(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(service.getPairByGroupId(id));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @PostMapping("pairs/create")
+    public ResponseEntity<?> createPair(@RequestBody Pairs pairs) {
+        try {
+            return ResponseEntity.ok(service.addPairs(pairs));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @PutMapping("/pairs/{id}/update")
+    public ResponseEntity<?> updatePairs(@PathVariable Integer id, @RequestBody Pairs pairs) {
+        try {
+            return ResponseEntity.ok(service.updatePairs(id, pairs));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
+
+    @DeleteMapping("/pairs/{id}/delete")
+    public ResponseEntity<?> deletePairs(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(service.deletePairs(id));
+        } catch (IllegalArgumentException e) {
+            JSONObject resp = new JSONObject();
+            resp.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(resp);
+        }
+    }
 }
+
--- lab2/src/main/java/ru/vsu/ru/zmaev/lab2/services/PairsService.java
+++ lab2/src/main/java/ru/vsu/ru/zmaev/lab2/services/PairsService.java
@@ -1,9 +1,13 @@
 package ru.vsu.ru.zmaev.lab2.services;
 
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.vsu.ru.zmaev.lab2.models.Pairs;
 import ru.vsu.ru.zmaev.lab2.repositories.PairsRepository;
 
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -29,5 +33,26 @@ public class PairsService {
 
     public Pairs getPairByGroupId(Integer id) {
         return repository.findByStudentGroupId(id).orElseThrow(() -> new IllegalArgumentException("Nu such pair"));
+    }
+
+    public Pairs addPairs(Pairs pairs) {
+        return repository.save(pairs);
+    }
+
+    public Pairs updatePairs(Integer id, Pairs pairs) throws IllegalArgumentException {
+        return repository.findByPairsId(id).map(p -> {
+            p.setPairsId(pairs.getPairsId());
+            p.setSubject(p.getSubject());
+            p.setStudentGroup(p.getStudentGroup());
+            p.setTimeSeries(LocalTime.MAX);
+            return repository.save(p);
+        }).orElseThrow(() -> new IllegalArgumentException("No such pair"));
+    }
+
+    public ResponseEntity<?> deletePairs(Integer id) throws IllegalArgumentException {
+        return repository.findByPairsId(id).map(p -> {
+            repository.delete(p);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new IllegalArgumentException("No such pair"));
     }
 }
